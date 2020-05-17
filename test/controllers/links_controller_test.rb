09969ce94f7e3d910a50a1196247faa26d51fa1c
Swastikyadav <swastikyadav.dev@gmail.com>
2020-05-17 20:26:40 +0530

    wrote test cases for controller

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -4,14 +4,24 @@ class LinksControllerTest < ActionDispatch::IntegrationTest
   fixtures :links
 
   test "should create new url with slug" do
-    post links_path, params: {link: { url: "https://testurl.com" } }
+    status_code = post links_path, params: {link: { url: "https://testurl.com" } }
     assert_response :success
-    json_response = JSON.parse(response.body)
-    assert_equal true, json_response["success"]
   end
 
   test "should get show" do
     get link_path(links(:one).slug)
     assert_response :success
+  end
+
+  test "should get index and return correct response" do
+    get links_path
+    assert_response :success
+  end
+
+  test "should update and return correct response" do
+    patch link_path(links(:one).slug), params: { link: { pinned: true } }
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_equal true, json_response["links"][0]["pinned"]
   end
 end
