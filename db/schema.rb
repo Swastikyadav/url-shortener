f3502097df9616db63a262d2f253eb555bb788ec
Swastikyadav <swastikyadav.dev@gmail.com>
2020-05-17 19:44:02 +0530

    changed DB from sqlite to PostgresQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_17_081526) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.string "slug"
