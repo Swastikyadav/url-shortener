384f0c36c12a89dedadb51ce407976e53e5e69df
Swastikyadav <swastikyadav.dev@gmail.com>
2020-05-17 09:59:20 +0530

    inherited ApplicationController from Base instead of API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
