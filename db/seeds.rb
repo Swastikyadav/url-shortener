2cd89d686fd2de53d329a103bf7e9bba6c9e5fc3
Swastikyadav <swastikyadav.dev@gmail.com>
2020-05-18 10:16:03 +0530

    populated seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+urls = [
+  "https://swastikyadav.com/learn",
+  "https://twitter.com/codeSwastik",
+  "https://learnrubyonrails.bigbinary.com",
+  "https://altcampus.io/batches/9/students",
+  "https://freecodecamp.com/learn/courses",
+  "https://hashnode.com/post/seeding-your-db",
+  "https://github.com/Swastikyadav/url-shortener",
+  "https://web.whatsapp.com/profile",
+  "https://andromeda.com/secret-galaxy",
+  "https://canva.com/profile/your-designs"
+]
+
+def generate_slug
+  loop do
+    slug = SecureRandom.alphanumeric(8)
+    break slug unless Link.where(slug: slug).exists?
+  end
+end
+
+Link.destroy_all
+
+urls.each do |url|
+  Link.create(url: url, slug: generate_slug)
+end
